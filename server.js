ffb036f5714ec7e6e9261aa5eceb280cbe1bf808
Halyna Marchenko <marchenkohalyna888@gmail.com>
2025-01-27 16:30:56 +0100

    updating proxi server

---
+++ server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import axios from "axios";
+import cors from "cors";
+
+const app = express();
+const PORT = 3001;
+
+app.use(cors());
+
+app.get("/proxy", async (req, res) => {
+  const { url } = req.query;
+
+  if (!url) {
+    return res.status(400).json({ error: "URL is required" });
+  }
+
+  try {
+    const response = await axios.get(url, { responseType: "arraybuffer" });
+    res.setHeader("Content-Type", response.headers["content-type"]);
+    res.send(response.data);
+  } catch (error) {
+    res.status(error.response?.status || 500).json({ error: error.message });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Proxy server is running on http://localhost:${PORT}`);
+});
