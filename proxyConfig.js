8d650b851f0065a0a846919bb5712a7c6b885821
Halyna Marchenko <marchenkohalyna888@gmail.com>
2025-02-23 21:33:24 +0100

    updating configs

---
+++ proxyConfig.js
@@ -0,0 +1,27 @@
+// import dotenv from "dotenv";
+
+// dotenv.config();
+
+// // Массив прокси из Webshare
+// const PROXIES = [
+//   { host: "198.23.239.134", port: 6540 },
+//   { host: "207.244.217.165", port: 6712 },
+//   { host: "107.172.163.27", port: 6543 },
+//   { host: "64.137.42.112", port: 5157 },
+// ];
+
+// const getRandomProxy = () =>
+//   PROXIES[Math.floor(Math.random() * PROXIES.length)];
+
+
+// const { host, port } = getRandomProxy();
+
+// export const proxyConfig = {
+//   host,
+//   port,
+//   auth: {
+//     username: process.env.PROXY_USERNAME, 
+//     password: process.env.PROXY_PASSWORD, 
+//   },
+//   protocol: "http", 
+// };
